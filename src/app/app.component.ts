8a3a65ba6e52295b6778263bff826914380056e8
yamanerkam <erkamyaman35@gmail.com>
2024-09-08 00:23:30 +0300

    json-slice-percent-titlecase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,12 @@ export class AppComponent {
   money:number = 50;
   name:string = "kam";
   today:Date= new Date();
+  titleOfTheBook : string = "the lord of the rings"
+
+
+  loveYouPlus1(){
+    for (let index = 0; index < index+1; index++) {
+      console.log("love you + 1")
+    }
+  }
 }
